aad97197446558e1ca3f06e41c9a3394d6532323
Vera Lippgardt <vera1078@mail.ru>
2022-08-25 22:20:14 +0300

    Координаты точек по номеру четверти

---
+++ zadacha18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 18.  Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y)
+
+System.Console.Write("Ведите номер четверти: ");
+int Quarter = int.Parse(Console.ReadLine()!);
+
+switch (Quarter)
+{
+    case 1:
+    System.Console.WriteLine("В первой четверти x>0 и y>0 ");
+    break;
+
+     case 2:
+    System.Console.WriteLine("Во второй четверти x<0 и y>0 ");
+    break;
+
+     case 3:
+    System.Console.WriteLine("В третьей четверти x<0 и y<0 ");
+    break;
+
+     case 4:
+    System.Console.WriteLine("В четвертой четверти x>0 и y<0 ");
+    break;
+}
+
